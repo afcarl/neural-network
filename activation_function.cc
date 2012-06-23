56cf95ee8040fca17567a5fb87584fcdd657b8d1
Mickael Lebedev <mickael.lebedev@gmail.com>
2012-06-23 19:43:05 +0200

    adding heavyside activation function

--- activation_function.cc
+++ activation_function.cc
@@ -6,6 +6,12 @@
 #include <math.h>
 #include "./activation_function.h"
 
+using namespace NeuralNetwork;
+
 double ActivationFunction::sigmoid(double x) {
   return (1 / (1 + exp(-x)));
+}
+
+double ActivationFunction::heavyside(double x) {
+	return 1 ? x >= 0 : 0;
 }
--- activation_function.h
+++ activation_function.h
@@ -6,9 +6,13 @@
 #ifndef ACTIVATION_FUNCTION_H_
 #define ACTIVATION_FUNCTION_H_
 
+namespace NeuralNetwork {
+
 class ActivationFunction  {
  public:
   static double sigmoid(double x);
+  static double heavyside(double x);
 };
+}
 
 #endif  // ACTIVATION_FUNCTION_H_
